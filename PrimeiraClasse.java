6af7f8936204b07bfe5c53f0d4f2efde6094ba18
Paulo André <andrelimarrc@gmail.com>
2023-10-19 20:54:39 -0300

    atualizando

---
+++ PrimeiraClasse.java
@@ -0,0 +1,16 @@
+package Br.andrelima;
+
+public class PrimeiraClasse {
+
+    public static void main(String args[]) {
+
+
+            System.out.print("Hello Paulo André");
+
+
+        }
+
+
+
+
+}
