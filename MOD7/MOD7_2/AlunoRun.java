6bfd79b23305975ebe5d64b41d4e114f227e8668
Paulo André <andrelimarrc@gmail.com>
2023-10-25 12:04:17 -0300

    atualizando

---
+++ MOD7/MOD7_2/AlunoRun.java
@@ -0,0 +1,21 @@
+package MOD7_2;
+
+public class AlunoRun {
+
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        aluno.setEscola("Ebac");
+        aluno.setNome("Paulo André");
+        aluno.setIdade(20);
+       System.out.println("O aluno se chama "+aluno.getNome()+" e está na escola "+aluno.getEscola()+" e possui a idade de "+aluno.getIdade()+" anos");
+
+
+
+    }
+
+//poderia me explicar o que é o objeto do mundo real? que o prof disse. Obrigado.
+
+
+
+}
