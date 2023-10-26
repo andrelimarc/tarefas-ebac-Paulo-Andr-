c23e2bd9ebbd5a7c88fb2c7d675676887b17a0ef
Paulo André <andrelimarrc@gmail.com>
2023-10-26 11:28:47 -0300

    adicionando mod8

---
+++ MOD8/TarefaMod8.java
@@ -0,0 +1,23 @@
+package TreinoMod7;
+
+public class TarefaMod8 {
+
+
+    private String nome;
+    private double nota;
+
+//Gostaria de saber como seria o uso do getter and setter aqui.
+
+    public void calculando(double nota1, double nota2, double nota3, double nota4){
+
+        double media = nota1 + nota2 + nota3 + nota4;
+      this.nota = media / 4;
+      System.out.println(this.nota);
+
+
+    }
+
+
+
+
+}
---
+++ MOD8/TarefaMod8Run.java
@@ -0,0 +1,19 @@
+package TreinoMod7;
+
+public class TarefaMod8Run {
+
+    public static void main(String[] args) {
+
+        TarefaMod8 t1 = new TarefaMod8();
+        t1.calculando(10,6,7,8);
+
+
+
+
+    }
+
+
+
+
+    }
+//Nessa tarefa colocar em pratica calculo, a leitura de 4 notas e dividir por 4. A media do resultado e a impressão no console.
