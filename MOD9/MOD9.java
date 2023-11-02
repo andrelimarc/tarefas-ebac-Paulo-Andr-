9572c34a95014c6f8124dafe8cd0ddf656fa14e1
Paulo André <andrelimarrc@gmail.com>
2023-11-02 12:13:38 -0300

    adicionando tarefa mod9

---
+++ MOD9/MOD9.java
@@ -0,0 +1,39 @@
+package tarefaMOD9;
+
+public class MOD9 {
+
+private double x = 5;
+private Double y = x;
+
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+
+    public void conversao(){
+
+        System.out.println(this.y);
+
+
+
+
+
+    }
+
+
+
+}
+
---
+++ MOD9/Mod9RUN.java
@@ -0,0 +1,21 @@
+package tarefaMOD9;
+
+public class Mod9RUN {
+
+
+
+    public static void main(String[] args) {
+
+     MOD9 mod9 = new MOD9();
+    mod9.conversao();
+
+
+
+
+
+    }
+
+
+
+
+}
